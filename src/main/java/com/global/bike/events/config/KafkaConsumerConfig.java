10b161d4d8bfa01ab33e663890988e92b3eaaa59
makesh1902 <makesharvindan@gmail.com>
2023-04-22 16:17:28 +0530

    Kafka consumer with Deserializer and manual acknowledgement

--- src/main/java/com/global/bike/events/config/KafkaConsumerConfig.java
+++ src/main/java/com/global/bike/events/config/KafkaConsumerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,7 +34,7 @@ public class KafkaConsumerConfig {
 
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://localhost:9092");
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         configs.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
 
         return new DefaultKafkaConsumerFactory<>(configs);
